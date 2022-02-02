e60a6d490a83920de4a28f327ca0f1697c9fb080
Shubham Das <skshubhamdas@gmail.com>
2022-02-01 23:24:26 -0800

    added user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+    include SessionsHelper
+
+    def new
+    end
+
+    def create
+        @user = User.new(user_params)
+        @user.profile = Profile.new
+
+        if @user.save
+            log_in(@user)
+            redirect_to(edit_path)
+            flash[:success] = "Registered successfully!"
+
+        else
+            flash.now[:danger] = 'An error occured'
+            render('new')
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password, :confirm_password)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
+  get    'signup'  => 'users#new'
+  post   'signup'  => 'users#create'
 
   resources :profiles , only: [:update]
   resources :educations , only: [:new]
