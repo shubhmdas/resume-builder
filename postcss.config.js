12981262b8fa83d4c5a8d24f8cedc40031c93ff2
Shubham Das <skshubhamdas@gmail.com>
2022-02-02 03:37:35 -0800

    modified postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   plugins: [
-    require('tailwindcss')("./app/javascript/stylesheets/tailwind.config.js"),
     require('autoprefixer'),
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
